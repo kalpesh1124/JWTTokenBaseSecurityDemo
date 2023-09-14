929c5f8808039f11d2861fcc7817f41b500dc314
kalpesh1124 <kalpeshinde170@gmail.com>
2023-09-15 00:10:09 +0530

    Initial commit

---
+++ JWTTokenBaseSecurityDemo/JWTTokenBaseSecurityDemo/Controllers/StudentDataController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using JWTTokenBaseSecurityDemo.Models;
+
+namespace JWTTokenBaseSecurityDemo.Controllers
+{
+      [Authorize]
+    public class StudentDataController : ApiController
+    {
+        ApiConsumeDemoEntities db;
+       public StudentDataController()
+       {
+           db=new ApiConsumeDemoEntities();
+       }
+       [HttpGet]
+       [Route("api/Student")]
+       public List<Student> GetAllStudent()
+       {
+           return db.Students.ToList();
+       }
+
+    }
+}
---
+++ JWTTokenBaseSecurityDemo/JWTTokenBaseSecurityDemo/Models/MyAuthProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace JWTTokenBaseSecurityDemo.Models
+{
+    public class MyAuthProvider : OAuthAuthorizationServerProvider
+    {
+
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            ApiConsumeDemoEntities obj = new ApiConsumeDemoEntities();
+            var userdata = obj.spCheckLogin(context.UserName, context.Password).FirstOrDefault();
+            if (userdata != null)
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, userdata.roleName));
+                identity.AddClaim(new Claim(ClaimTypes.Name, userdata.employeeCode));
+                identity.AddClaim(new Claim(ClaimTypes.MobilePhone, userdata.mobile_number));
+
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Provided username and password is incorrect");
+                context.Rejected();
+            }
+        }
+
+    }
+}
